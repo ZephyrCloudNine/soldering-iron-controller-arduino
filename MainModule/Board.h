fae218f093030eb6401d744cb451a632340e3b76
Milinda Jayamaha <donmilinda@gmail.com>
2023-05-23 21:14:18 +0530

    added pinouts to Board.h config file

--- MainModule/Board.h
+++ MainModule/Board.h
@@ -2,8 +2,14 @@
 #define BOARD_H
 
 //Stores pin defines
+#define BUTTON_PIN A3
 
+#define ENC_A_PIN 12
+#define ENC_B_PIN 11
+#define ENC_SW_PIN 10
 
+#define THERMISTOR_PIN A1
 
+#define HEATER_PIN A5
 
 #endif
