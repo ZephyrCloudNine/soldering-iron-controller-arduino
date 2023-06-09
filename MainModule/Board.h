edb2285efb6b920e6aa77b224d816215b02b248e
Milinda Jayamaha <donmilinda@gmail.com>
2023-06-09 19:47:58 +0530

    bugfix - fixed wrong pin numbers for button and encoder

--- MainModule/Board.h
+++ MainModule/Board.h
@@ -2,11 +2,11 @@
 #define BOARD_H
 
 //Stores pin defines
-#define BUTTON_PIN A3
+#define BUTTON_PIN A4
 
-#define ENC_A_PIN 12
+#define ENC_A_PIN 10
 #define ENC_B_PIN 11
-#define ENC_SW_PIN 10
+#define ENC_SW_PIN 12
 
 #define THERMISTOR_PIN A1
 
